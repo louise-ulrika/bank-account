ac5489b7a4393de5ebf34e2d692a88de21a68d01
louise-ulrika <ulrika.nicander@gmail.com>
2025-12-02 20:40:44 +0100

    changes but not done
    a couple of changes, but not done

--- script/script.js
+++ script/script.js
@@ -1,2 +1,25 @@
-const accountName = {"Chloe Matthews"}
-    
+const account = {
+    accountName:"Chloe Matthews",
+    balance:41000,
+    getBalance:function(){
+        console.log(this.balance);
+    },
+    deposit(amount){
+        this.balance += amount;
+    },
+    withdrawal(amount){
+        this.balance -= amount;
+
+    },
+    getAccountName(){
+        console.log(this.accountName);
+    },
+    accountError(){
+        console.log("Error: Account not found.");
+    },   
+
+    exitAccount(){
+        console.log("Exiting account...");
+    },
+}
+account.getBalance(); 
