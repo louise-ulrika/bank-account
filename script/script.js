2e248bfbbe2de7a9aa7c3b4776e681ff20fea1b7
louise-ulrika <ulrika.nicander@gmail.com>
2025-12-03 13:57:23 +0100

    Changes made 13.57
    changes wednesday 13.57

--- script/script.js
+++ script/script.js
@@ -5,20 +5,27 @@ const account = {
         console.log(this.balance);
     },
     deposit(amount){
+        const message= parseFloat(prompt("Enter deposit amount:"));
         this.balance += amount;
     },
     withdrawal(amount){
+        const message= parseFloat(prompt("Enter withdrawal amount:"));
         this.balance -= amount;
 
     },
     getAccountName(){
+        const message= parseFloat(prompt("Enter your account name:"));
         console.log(this.accountName);
     },
     accountError(){
+        const message= parseFloat(prompt("Enter your account name:"));
         console.log("Error: Account not found.");
     },   
 
     
-    }
+    };
 
 account.getBalance(); 
+account.getAccountName();
+account.deposit(5000);
+account.withdrawal(2000);
